425e219f75ef5e49874cf6e504ac168107aef11a
briws <briws@pm.me>
2020-11-30 19:48:15 +0300

    lesson 22 complete

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,14 +1,15 @@
 import s from './Dialogs.module.css'
 import React from "react";
+import {NavLink} from "react-router-dom";
 
 const Dialogs = (props) => {
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>
-        <div className={s.dialog}>Andrey</div>
-        <div className={s.dialog}>Dmitry</div>
-        <div className={s.dialog}>Sasha</div>
-        <div className={s.dialog + " " + s.active}>Sveta</div>
+        <div className={s.dialog}><NavLink to='/dialogs/1'>Andrey</NavLink></div>
+        <div className={s.dialog}><NavLink to='/dialogs/2'>Dmitry</NavLink></div>
+        <div className={s.dialog}><NavLink to='/dialogs/3'>Sasha</NavLink></div>
+        <div className={s.dialog + " " + s.active}><NavLink to='/dialogs/4'>Sveta</NavLink></div>
       </div>
       <div className={s.messages}>
         <div className={s.message}>Hi</div>
