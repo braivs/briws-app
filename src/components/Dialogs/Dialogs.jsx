5c277093b07e6585c817f138ee7a1ef392ce1fcd
briws <briws@pm.me>
2020-12-08 18:53:18 +0300

    mistake in previos commit: lesson 31

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,7 +3,6 @@ import s from './Dialogs.module.css'
 import DialogItem from "./DialogItem/DialogItem";
 import Message from "./Message/Message";
 
-
 const Dialogs = (props) => {
   let dialogs = props.state.dialogs.map(d => <DialogItem name={d.name} id={d.id}/>);
   let messages = props.state.messages.map(m => <Message author={m.author} message={m.message}/>);
